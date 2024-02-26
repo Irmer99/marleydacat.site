61d8607d50e9d489d8965704e619d8684ffd64b8
babypastorkayvon <47457302+babypastorkayvon@users.noreply.github.com>
2024-02-25 19:27:25 -0600

    m

--- backend/src/posting_routes.py
+++ backend/src/posting_routes.py
@@ -56,11 +56,11 @@ async def create_post(session_id:str=Cookie(None), session_storage=Depends(get_s
     if file_type not in ImageStorage.VALID_FILE_EXTENSIONS:
         raise HTTPException(status_code=422, detail="Invalid image file.")
     # use out vision model to try to make sure the image is a cat
-    r = requests.post('http://vision:8003/cat_classify', files={'image': contents})
-    if r.status_code != 200:
-        raise HTTPException(status_code=500, detail="classification error")
-    if not r.json()["is_cat"]:
-        raise HTTPException(status_code=418, detail="not a cat")
+    # r = requests.post('http://vision:8003/cat_classify', files={'image': contents})
+    # if r.status_code != 200:
+    #     raise HTTPException(status_code=500, detail="classification error")
+    # if not r.json()["is_cat"]:
+    #     raise HTTPException(status_code=418, detail="not a cat")
     # store the file in our storage bucket
     storage = ImageStorage()
     await file.seek(0)
